d707782ed31fcf8928b088e743e1b346fcbcb85c
duihuhu <duihuhu@163.com>
2022-07-15 15:35:36 +0800

    alloc

--- ib.c
+++ ib.c
@@ -201,7 +201,8 @@ int init_ib()
 		goto init_ib_exit;
 	}
 	res.ib_buf_size = cfg.msg_size;
-	res.ib_buf = (char *) memalign(PAGE_SIZE, res.ib_buf_size);
+	// res.ib_buf = (char *) memalign(PAGE_SIZE, res.ib_buf_size);
+	res.ib_buf = (char *) malloc(res.ib_buf_size);
 	if (!res.ib_buf) {
 		fprintf(stdout, "alloc buffer failed\n");
 		goto init_ib_exit;
