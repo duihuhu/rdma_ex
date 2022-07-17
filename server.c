eeee751b356191486d994470b94c6f6aaf8ab7cd
duihuhu <duihuhu@163.com>
2022-07-17 10:26:56 +0800

    fix

--- server.c
+++ server.c
@@ -24,19 +24,19 @@ int run_server (struct Resource *res, int sockfd)
     pthread_attr_setdetachstate (&attr, PTHREAD_CREATE_JOINABLE);
 
     threads = (pthread_t *) calloc (cfg.num_threads, sizeof(pthread_t));
-    check (threads != NULL, "Failed to allocate threads.");
-
+    if (threads == NULL)
+        fprintf(stderr,  "Failed to allocate threads.")
     for (i = 0; i < cfg.num_threads; i++) {
         mul_args.thread_id = i;
         ret = pthread_create (&threads[i], &attr, server_func, (void *)&mul_args);
-        if (ret == 0) 
+        if (ret != 0) 
             fprintf(stderr, "Failed to create server_thread[%ld]", i);
     }
 
     bool thread_ret_normally = true;
     for (i = 0; i < cfg.num_threads; i++) {
         ret = pthread_join (threads[i], &status);
-        if (ret == 0) 
+        if (ret != 0) 
             fprintf(stderr, "Failed to join thread[%ld].", i);
         if ((long)status != 0) {
             thread_ret_normally = false;
