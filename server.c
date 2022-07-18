6cfeea52bf9396675d6e9fd6d0206b385023532f
duihuhu <duihuhu@163.com>
2022-07-18 11:34:07 +0800

    fix t

--- server.c
+++ server.c
@@ -33,13 +33,14 @@ int run_server (struct Resource *res)
     struct MulArgs *mul_args;
     mul_args = malloc(cfg.num_threads * sizeof(struct MulArgs));
     pthread_t   *threads = NULL;
-    threads = (pthread_t *) calloc (cfg.num_threads, sizeof(pthread_t));
-    if (threads == NULL)
-        fprintf(stderr,  "Failed to allocate threads.");
+    void    *status;
+
     pthread_attr_t  attr;    
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-
+    threads = (pthread_t *) calloc (cfg.num_threads, sizeof(pthread_t));
+    if (threads == NULL)
+        fprintf(stderr,  "Failed to allocate threads.");
     struct sockaddr_in c_addr;
     int *listenfd;
     listenfd = (int *)malloc(cfg.num_threads * sizeof(int));
